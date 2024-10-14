554017b54b475f6248b6ef320bf6f6954e8d09b8
Aryan Singh <83677526+aru24sin@users.noreply.github.com>
2024-10-13 20:33:25 -0400

    Progress towards python practice files

--- Q1.py
+++ Q1.py
@@ -1,5 +1,14 @@
 #Variables & Data Types
 
-#Initialize each data type in python as a unique variable
+#1) Initialize each data type in python as a unique variable
 #Follow the rules for variable names
 #Output each created variable using the print() function
+
+#2) Create two string variables s1 & s2
+
+#Create a new variable s3 that adds s1 & s2 together
+#print this new variable out
+
+#This is called concatenation,
+#in python you are able to add two objects of the same data type together to combine them
+
--- Q2.py
+++ Q2.py
@@ -1,5 +1,16 @@
-#Variables & Data Types
+#Lists & Dictionaries
 
-#Initialize each data type in python as a unique variable
-#Follow the rules for variable names
-#Output each created variable using the print() function
+#Given the following list
+l1 = ["John", 'F', 2, 5.03, True]
+
+#print out the first element
+
+#print out the last element using negative index
+
+#print out elements 2 through 4
+
+#change the first element of the list to your first name
+
+#add a new element to the list by using the append() method Ex: temp.append(3)
+
+#create a new variable that contains a copy of the list
