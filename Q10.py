71cfa2114b7a11679a2d600d7212f8abca020bc0
Aryan Singh <83677526+aru24sin@users.noreply.github.com>
2024-10-13 22:31:59 -0400

    Complete python practice questions within files

---
+++ Q10.py
@@ -0,0 +1,11 @@
+#Modules & Libraries
+
+import matplotlib.pyplot as plt
+
+#1) Use the pip install method to install matplotlib,
+# Use the two lists below to plot a graph using plt.plot()
+# Add labels to x-axis and y-axis using plt.xlabel("label name")
+# Display the plot using the plt.show() method
+
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
--- Q9.py
+++ Q9.py
@@ -1,11 +1,4 @@
 #Files
 
-import matplotlib.pyplot as plt
-
-#1) Use the pip install method to install matplotlib,
-# Use the two lists below to plot a graph using plt.plot()
-# Add labels to x-axis and y-axis using plt.xlabel("label name")
-# Display the plot using the plt.show() method
-
-x = [1, 2, 3, 4, 5]
-y = [2, 4, 6, 8, 10]
+#1) Open the file "Q9.txt", create a for loop to iterate through & print() each line out
+# close the file outside the loop
